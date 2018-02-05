421f5eeaec85e0c6c4ca7e9edf7a7898bd530717
jpsierens <jsierens@tacitknowledge.com>
2018-02-05 17:17:20 -0600

    yee

--- lesson1/notes.md
+++ lesson1/notes.md
@@ -1,2 +1,11 @@
 # 3
 A eulerian (pronounced oilerian) path is a path that follows the edges of a graph, visiting all the nodes and hitting an edge only once.
+
+# 8 Russian peasants algorithms
+this introduces the concept of bit shifting helpful for this section:
+https://stackoverflow.com/a/141873
+
+17 >> 1 is graphically explained by:
+10001 -> 01000
+
+it halves the number, rounded.
---
+++ lesson1/russian.py
@@ -0,0 +1,30 @@
+def russian(a,b):
+    x = a; y = b
+    z = 0
+    while x > 0:
+        if x % 2:
+            z = z + y
+        y = y << 1 #this doubles the number
+        x = x >> 1 #this halves the number, rounded.
+    return z
+
+print(russian(10, 5))
+
+#first pass:
+# z = 0
+# y = 10
+# x = 5
+# second pass:
+# z = 10
+# y = 20
+# x = 2
+# third pass:
+# z = 10
+# y = 40
+# x = 1
+# fourth pass:
+# z = 50
+# y = 80
+# x = 0
+#
+# answer = 50!
