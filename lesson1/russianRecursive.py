faf9137efaf8450da9c20f2c4eb80cdd28013ad6
jpsierens <jpsierens1988@gmail.com>
2018-02-13 17:31:29 -0600

    second lesson

--- lesson1/russianRecursive.py
+++ lesson1/russianRecursive.py
@@ -4,3 +4,7 @@ def russian(a,b):
     return b + 2*russian((a-1)/2, b)
 
 print(russian(5, 10))
+
+# divide and conquer
+# it splits up the multiplication, making it easier each time to compute, then just
+# multiplying by 2 (doubling) (which isnt costly)
